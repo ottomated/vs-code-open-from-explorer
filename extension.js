067b2a89d0200eb505ea8139bc39a832fb76e1c7
Ottomated <otto@ottomated.net>
2020-01-19 21:18:05 -0800

    Release 0.1.0

--- extension.js
+++ extension.js
@@ -1,35 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
+const exec = require('child_process').execSync;
+const fs = require('fs');
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "open-from-explorer" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+	let disposable = vscode.commands.registerCommand('extension.openfile', (file) => {
+		let type = exec(`xdg-mime query filetype "${file.path}"`).toString().trim();
+		if (!type)
+			return vscode.window.showErrorMessage(`Could not determine filetype.`);
+		let app = exec(`xdg-mime query default "${type}"`).toString().trim();
+		if (!app)
+			return vscode.window.showErrorMessage(`Could not determine default app.`);
+		try {
+			exec(`xdg-open "${file.path}"`);
+			vscode.window.showInformationMessage(`Opening in ${getAppName(app)}.`);
+		} catch (e) {
+			return vscode.window.showErrorMessage(`Could not open file: ${e.message.trim()}.`);
+		}
 	});
 
 	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
-// this method is called when your extension is deactivated
-function deactivate() {}
+function getAppName(desktop) {
+	let possibilities = [`/usr/share/applications/${desktop}`, `${process.env.HOME}/.local/share/applications/${desktop}`];
+	for (let file of possibilities) {
+		if (fs.existsSync(file)) {
+			let entry = fs.readFileSync(file, 'utf8');
+			let match = entry.match(/Name=(.+)/);
+			if (match !== null) {
+				return match[1];
+			}
+		}
+	}
+	return desktop.replace('.desktop', '');
+}
+
+function deactivate() { }
 
 module.exports = {
 	activate,
